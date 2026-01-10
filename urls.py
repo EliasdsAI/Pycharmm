edadf041a25e388b5957b08cfa44572c26bb22a7
EliasdsAI <jedantas64@gmail.com>
2026-01-09 21:31:53 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from cliente.views import cadastrar_cliente
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('cliente.urls')),
+    path('', include('principal.urls'))
+
 ]
